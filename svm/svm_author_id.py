318642c8ec040530f5ff66cd38b37edf76cfceb1
Aditya Kaushik <adityakaushik471@gmail.com>
2018-05-26 07:16:07 +0530

    Solved SVM mini project and fix email_preprocess

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/home/aadi/prog/udacityProjects/ml_udacity/ud120-projects/tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
@@ -24,7 +24,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+
+# CLassifier
+# clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C=10000)
+# features_train = features_train[:len(features_train)//100]
+# labels_train = labels_train[:len(labels_train)//100]
+print('time before fitting up the model is ')
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+# Predicting the result.
+print('Time before prediciting...')
+t1 = time()
+pred = clf.predict(features_test)
+print('Predicting Time:', round(time()-t1, 3), 's')
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print("Accuracy of our classifier is ", accuracy)
+
+count = 0
+for i in pred:
+    if i==1: count+=1
+print("Number of event in favor of chris(1) are " , count)
+
 
 #########################################################
-
-
